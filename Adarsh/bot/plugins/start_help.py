09f4e7cd1332401073ab7418f221173b2251c06f
Assaulter-Shiv <assaulter819@gmail.com>
2024-08-25 23:49:14 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -31,9 +31,7 @@ async def start(b, m):
                 [
                     [InlineKeyboardButton(" ᴜᴘᴅᴀᴛᴇs ", url="https://t.me/BoB_Files1"), InlineKeyboardButton(" ᴍᴏᴠɪᴇs ", url="https://t.me/REQUESTING_MOVIES_SERIES_GROUPS"),
                     [InlineKeyboardButton(" ᴏᴡɴᴇʀ ", url="https://t.me/Assaulter_Shiv"), InlineKeyboardButton(" ᴅᴇᴠᴇʟᴏᴘᴇʀ ", url="https://t.me/Assaulter_shiv")]
-                ]
-                 )
-            
+                    ]  
         )
     else:
 
@@ -87,9 +85,7 @@ async def help_handler(bot, message):
                     [InlineKeyboardButton(" ᴜᴘᴅᴀᴛᴇs ", url="https://t.me/BoB_Files1"), InlineKeyboardButton(" ᴍᴏᴠɪᴇs ", url="https://t.me/REQUESTING_MOVIES_SERIES_GROUPS"),
                     [InlineKeyboardButton(" ᴏᴡɴᴇʀ ", url="https://t.me/Assaulter_Shiv"), InlineKeyboardButton(" ᴅᴇᴠᴇʟᴏᴘᴇʀ ", url="https://t.me/Assaulter_shiv")]
 
-                ]
-                 )
-            
+                    ]
         )
 
 @StreamBot.on_message(filters.command('filmymen') & filters.private)
@@ -120,7 +116,5 @@ async def about_handler(bot, message):
                   [InlineKeyboardButton(" ᴜᴘᴅᴀᴛᴇs ", url="https://t.me/BoB_Files1"), InlineKeyboardButton(" ᴍᴏᴠɪᴇs ", url="https://t.me/REQUESTING_MOVIES_SERIES_GROUPS"),
                     [InlineKeyboardButton(" ᴏᴡɴᴇʀ ", url="https://t.me/Assaulter_Shiv"), InlineKeyboardButton(" ᴅᴇᴠᴇʟᴏᴘᴇʀ ", url="https://t.me/Assaulter_shiv")]
                
-                ]
-                 )
-            
+                  ]
         )
